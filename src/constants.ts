63cdbd4056fbb820bf318c3add85068a4b544ce4
Julien Hellio <jhellio56@gmail.com>
2019-04-25 11:32:22 +0200

    d

--- src/constants.ts
+++ src/constants.ts
@@ -4,5 +4,5 @@ export const messages = {
     goodbye: "A bientôt!",
     help: "Je peux faire XXX pour vous!",
     wesh: "Wesh!",
-    resultFoot: "https://www.thesportsdb.com/api/v1/json/1/eventsnextleague.php?id=4328";
+    resultFoot: "https://www.thesportsdb.com/api/v1/json/1/eventsnextleague.php?id=4328"
 }
